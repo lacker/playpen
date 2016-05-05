3282bf86c06deeb6a03ecc0fd2a7bd786398de1b
Kevin Lacker <lacker@gmail.com>
2016-05-05 13:44:49 -0700

    editor can edit but phone doesnt show anything

--- js/App.js
+++ js/App.js
@@ -2,29 +2,15 @@ import React, { Component } from 'react';
 import EditorPlayer from './EditorPlayer';
 
 const defaultValue = `import React, { AppRegistry, View, Text } from 'react-native'
-const style = {
-  flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center',
-}
 
-const textStyle = {
-  backgroundColor: 'whitesmoke',
-  color: 'teal',
-  fontSize: 24,
-  padding: 10,
-}
-
-const App = () => {
+const Root = () => {
   return (
-    <View style={style}>
-      <Text style={textStyle}>Hello!</Text>
-    </View>
+    <Text>Hello there</Text>
   )
 }
 
 // App registration and rendering
-AppRegistry.registerComponent('MyApp', () => App)`;
+AppRegistry.registerComponent('MyRoot', () => Root)`;
 
 export default class App extends Component {
   render() {
